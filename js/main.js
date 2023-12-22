732d215528742f746e632ddf2d7b20e883272878
Thiago Inacio <thiago.i.oliveira@aluno.senai.br>
2023-12-22 00:51:53 -0300

    ta dificil...

--- js/main.js
+++ js/main.js
@@ -1,28 +1,85 @@
-import {createProduto, } from "./services.js";
+import {getAllProducts, createProduct, updateProduct, deleteProduct} from "./services.js";
 
-
+window.onload = () => {
+    loadProducts();
+  };
   
+  const loadProducts = () => {
+    const dataContainer = document.getElementById("data-container");
+    getAllProducts().then((resp) => {
+      resp.forEach((produto) => {
+        const productsElement = document.createElement("div");
+        productsElement.innerHTML = `
+       
+        <div class="elemento">
+        <p class="descricao">${produto.descricao}</p> 
+    
+        <hr>
+       
+      <p class="und">${produto.und}</p> 
+      
+      <hr>
+    
+      <p class="ncm">${produto.ncm}</p>
+
+      <hr>
+
+      <p class="preco">${produto.preco}</p>
+      
+        </div>
+        `;
+
+        dataContainer.appendChild(productsElement);
+    });
+  });
+};
+
 document.getElementById('btn-salvar').addEventListener('click', () => {
-    
-    const descricao = document.getElementById('descricao').value;
-    const und = document.getElementById('und').value;
-    const ncm = document.getElementById('ncm').value;
-    const preco = document.getElementById('preco').value;
-    
-    
-
-
-    const dataToSend = {
+    const produto = {
         descricao: descricao,
         und: und,
         ncm: ncm,
-        preco: preco
+        preco: preco,
+    };
+      
+
+
+    const dataToSend = {
+    descricao: descricao,
+    und: und,
+    ncm: ncm,
+    preco: preco
 
     };
 
-    createProduto(dataToSend);
+    createProduct(produto);
 
+});
 
     // alert(JSON.stringify(dataToSend))
+
+    document.getElementById("btn-atualizar").addEventListener("click", () => {
+        const produto = {
+          descricao: "ALFAJOR RECHEADO",
+          und: "UN",
+          ncm: "21069090",
+          preco: "25",
+          id: 3,
+        };
+
+        updateProduct(produto);
+
+    });
+
+    document.getElementById("btn-excluir").addEventListener("click", () => {
+        const produto = {
+            descricao: "ALFAJOR RECHEADO",
+            und: "UN",
+            ncm: "21069090",
+            preco: "25",
+            id: 3,
+        };
+        
+        deleteProduct(produto)
 
 });
--- js/services.js
+++ js/services.js
@@ -1,9 +1,23 @@
-import {handleErrors } from "./exception.js";
+import {handleErrors } from "./exceptions.js";
 var URL = 'http://localhost:3000/produtos';
 
+export const getAllProducts = async () => {
+    try {
+        const response = await fetch(URL);
 
+        // lidando com os erros na resposta
+        handleErrors(response);
 
-export const createProduto = async (produto) => {
+        // converter os dados para json
+       return await response.json();
+
+    
+    } catch (error) {
+        console.log('Error >>>', error);
+    }
+};
+
+export const createProduct = async (produto) => {
     fetch(URL, {
         method: 'POST',
         headers: {
@@ -12,7 +26,27 @@ export const createProduto = async (produto) => {
         body: JSON.stringify(produto)
     })
     .then(response => response.json())
-    .then(data => console.log('success: ', data))
-    .catch((erro) => console.log('Error: ', error));
+    .then(data => console.log('Success: ', data))
+    .catch((error) => console.log('Error: ', error));
 
+};
+
+export const deleteProduct = async (produto) => {
+    fetch(URL+`/${produto.id}`, { method: 'DELETE'})
+    .then(response => response.json())
+    .then(data => console.log('Success: ', data))
+    .catch((error) => console.log('Error: ', error));
+};
+
+export const updateProduct = async (produto) => {
+    fetch(URL+`/${produto.id}`, {
+        method: 'PATCH',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(produto)
+    })
+    .then(response => response.json())
+    .then(data => console.log('Success: ', data))
+    .catch((error) => console.log('Error: ', error));
 };
